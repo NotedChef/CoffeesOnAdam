5d46208916a48ce8b8778d99a182ecb9cbb21dc2
Barry Sanders <barry_ande@hotmail.com>
2017-10-29 16:26:48 +1000

    Changed layout of order-edit page

--- src/app/order/order-list/order-list.component.ts
+++ src/app/order/order-list/order-list.component.ts
@@ -1,24 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { OrderService } from '../order.service';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { Order } from '../order';
 import { AuthService } from '../../auth/auth.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
   styleUrls: ['./order-list.component.css']
 })
-export class OrderListComponent implements OnInit {
-  orders$: FirebaseListObservable<Order[]>;
+export class OrderListComponent implements OnInit, OnDestroy {
+  //orders$: FirebaseListObservable<Order[]>
+  orders: Array<Order> = null;
+  ordersSubscription: Subscription;
 
   constructor(private orderService: OrderService, private authService: AuthService) {
     this.orderService.subject$.next(this.authService.currentUserId);
-    this.orders$ = this.orderService.getOrdersByUserId();
+    
   }
 
   ngOnInit() {
-     this.orders$.subscribe(o => console.log('Orders: ', o));
+    this.ordersSubscription = this.orderService.getOrdersByUserId()
+      .subscribe(o => {
+          console.log(o);
+          this.orders = o;
+      });
+  }
+
+  ngOnDestroy() {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.ordersSubscription.unsubscribe();
   }
 
 }
